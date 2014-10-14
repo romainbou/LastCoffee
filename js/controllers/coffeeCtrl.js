0b52038f579555ddb2dc6f8e4f572e3766f3715e
Eric Allard <eric.allard0104@gmail.com>
2014-10-14 20:23:26 +0200

    dark-blue design

--- js/controllers/coffeeCtrl.js
+++ js/controllers/coffeeCtrl.js
@@ -5,38 +5,42 @@ Number.prototype.mod = function(n) {
     // (-9).mod(24) === 15 :)
     return ((this%n)+n)%n; 
 };
-var CustomTime = function(h, m) {
-  this.h = h;
-  this.m = m;
-  this.getTime = function() {
-    return parseInt(this.h) + parseFloat(this.m);
-  };
-}; 
 
-var lastCoffeeApp = angular.module('lastCoffeeApp', []);
+var lastCoffeeApp = angular.module('lastCoffeeApp', ['mgcrea.ngStrap']);
+
 lastCoffeeApp.controller('CoffeeCtrl', ['$scope', '$http',
   function($scope, $http) {
     $scope.max = 9;
     $scope.nbCoffee = 0;
-    $scope.lastCoffeeTime = new CustomTime('0','0');
-    $scope.bedTime = new CustomTime('0','0');
+    $scope.lastCoffeeTime = 36000000;
+    $scope.bedTime = 36000000;
+    $scope.forbidden = false;
+    $scope.changeNbCoffee = function(nb){
+      $scope.nbCoffee = nb;
+    }
     $scope.getNumber = function(num) {
-        return new Array(num);   
+        return new Array(num);
     }
     $scope.getStillWakeTime = function(){
-      return ($scope.bedTime.getTime() - $scope.lastCoffeeTime.getTime()).mod(24);
+      return ((($scope.bedTime / 3600000)+1) - (($scope.lastCoffeeTime / 3600000)+1)).mod(24);
     }
     $http.get('data/result.json').success(function(data) {
       $scope.results = data;
       $scope.getResult = function(){
         var stillWake = $scope.getStillWakeTime();
-        if($scope.nbCoffee>7)
+        if($scope.nbCoffee>7){
+          $scope.forbidden = true;
           return $scope.results.toomany;
-        if(stillWake<5)
+        }else if(stillWake<5){
+          $scope.forbidden = true;
           return $scope.results.toolate;
-        if($scope.nbCoffe>5 && $scope.nbCoffe<8 && stillWake>6)
+        }else if($scope.nbCoffe>5 && $scope.nbCoffe<8 && stillWake>6){
+          $scope.forbidden = true;
           return $scope.results.okayish;
-        return $scope.results.ok;
+        }else{
+          $scope.forbidden = false;
+          return $scope.results.ok;
+        }
       }
     });
   }
