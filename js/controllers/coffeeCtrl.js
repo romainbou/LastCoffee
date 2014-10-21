f99d519f936dcc2484bc205248844b412580001f
Romain Bouyé <romain@bouye.fr>
2014-10-21 20:23:41 +0200

    Firefox OS application LastCoffee Release 0.1

--- js/controllers/coffeeCtrl.js
+++ js/controllers/coffeeCtrl.js
@@ -1,13 +1,13 @@
 'use strict';
 (function(){
- 
+
   Number.prototype.mod = function(n) {
-      // -9 % 24 === -9 :( 
+      // -9 % 24 === -9 :(
       // (-9).mod(24) === 15 :)
-      return ((this%n)+n)%n; 
+      return ((this%n)+n)%n;
   };
 
-  var lastCoffeeApp = angular.module('lastCoffeeApp', ['LocalStorageModule', 'mgcrea.ngStrap']);
+  var lastCoffeeApp = angular.module('lastCoffeeApp', ['LocalStorageModule']);
 
   lastCoffeeApp.config(function (localStorageServiceProvider) {
     localStorageServiceProvider
@@ -24,25 +24,29 @@
       $ls.bind($scope, 'lastCoffeeTime');
       $ls.bind($scope, 'bedTime');
       $ls.bind($scope, 'nbCoffee');
-      
+
       var getCurrentTime = function(){
         var d = new Date();
-        var m = (Math.round(d.getMinutes()/$scope.minuteStep))/(60/$scope.minuteStep);
-        var h = d.getHours();
-        return (m+h-1)*3600000;
+        var m = d.getMinutes().toString();
+        var h = d.getHours().toString();
+        if(m.length <2)
+          m = '0'+m;
+        if(h.length <2)
+          h = '0'+h;
+        return h+':'+m;
       }
 
       //initialize with past values if exist or default values
       $scope.nbCoffee = $ls.get('nbCoffee') ||  0;
       $scope.lastCoffeeTime = $ls.get('lastCoffeeTime') ||  getCurrentTime();
-      $scope.bedTime = $ls.get('bedTime') || 75600000;
+      $scope.bedTime = $ls.get('bedTime') || '10:00';
 
       $scope.questionTemplates =
         [ { name: 'coffee-number', title:'coffee already drunk', model: 'nbCoffee', url: 'partials/coffee-number.html'},
           { name: 'last-coffee-time', title:'last coffee taken at', model: "lastCoffeeTime", url: 'partials/time.html'},
           { name: 'bedtime', title:'bedtime', model: "bedTime", url: 'partials/time.html'}];
 
-      
+
       $scope.changeNbCoffee = function(nb){
         $scope.nbCoffee = nb;
       }
@@ -54,7 +58,14 @@
         $scope.lastCoffeeTime = getCurrentTime();
       }
       var getStillAwakeTime = function(){
-        return ((($scope.bedTime / 3600000)+1) - (($scope.lastCoffeeTime / 3600000)+1)).mod(24);
+        var bedTime = $scope.bedTime.split(':');
+        var hBedTime = parseInt(bedTime[0]);
+        var mBedTime = parseInt(bedTime[1]);
+        var lastCoffeeTime = $scope.lastCoffeeTime.split(':');
+        var hLastCoffeeTime = parseInt(lastCoffeeTime[0]);
+        var mLastCoffeeTime = parseInt(lastCoffeeTime[1]);
+        return ((hBedTime + (mBedTime/60)) -(hLastCoffeeTime + (mLastCoffeeTime/60))).mod(24);
+        //return ((($scope.bedTime / 3600000)+1) - (($scope.lastCoffeeTime / 3600000)+1)).mod(24);
       }
       $http.get('data/result.json').success(function(data) {
         $scope.results = data;
@@ -64,7 +75,7 @@
           var result, conditionCoffee, conditionSleep;
           while(i<size && !match){
             result = data[i];
-            conditionCoffee = result.conditionCoffee; 
+            conditionCoffee = result.conditionCoffee;
             conditionSleep = result.conditionSleep;
             //Test if the conditions match with $scope.nbCoffee and stillAwakeTime :
             //conditionCoffee == [{"val":7, "ie" : "excl"}, null] => test if($scope.nbCoffee ∈  ]7,+∞[ )
